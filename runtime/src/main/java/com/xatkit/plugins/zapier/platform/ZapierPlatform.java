05e1a28a265e1751775e7c208c2c2c3779ebea73
Gwendal Daniel <gwendal.daniel@gmail.com>
2019-11-15 14:13:26 +0100

    integrate the new registerRestEndpoint API

--- runtime/src/main/java/com/xatkit/plugins/zapier/platform/ZapierPlatform.java
+++ runtime/src/main/java/com/xatkit/plugins/zapier/platform/ZapierPlatform.java
@@ -2,6 +2,7 @@ package com.xatkit.plugins.zapier.platform;
 
 import com.xatkit.core.XatkitCore;
 import com.xatkit.core.platform.RuntimePlatform;
+import com.xatkit.core.server.HttpMethod;
 import com.xatkit.core.server.RestHandlerFactory;
 import fr.inria.atlanmod.commons.log.Log;
 import org.apache.commons.configuration2.Configuration;
@@ -47,7 +48,7 @@ public class ZapierPlatform extends RuntimePlatform {
      */
     public ZapierPlatform(XatkitCore xatkitCore, Configuration configuration) {
         super(xatkitCore, configuration);
-        this.xatkitCore.getXatkitServer().registerRestEndpoint("/zapier/callback",
+        this.xatkitCore.getXatkitServer().registerRestEndpoint(HttpMethod.POST, "/zapier/callback",
                 RestHandlerFactory.createJsonRestHandler((headers, params, content) -> {
                     String actionId = content.getAsJsonObject().get(ACTION_ID_FIELD).getAsString();
                     String value = content.getAsJsonObject().get(VALUE_FIELD).getAsString();
